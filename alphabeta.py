1cc73288cdceed8c85101de1fd0895c9d84a3abd
Brian-T-Chan <69320248+Brian-T-Chan@users.noreply.github.com>
2021-03-21 17:39:58 -0600

    Update alphabeta.py

--- alphabeta.py
+++ alphabeta.py
@@ -20,6 +20,9 @@
 # Player B wins (in a way specified by the capturewin and noncapture win
 # functions in board.py) : 1
 
+# In addition to using the alpha-beta pruning algorithm, the following is
+# done to skew scores.
+
 # If neither Player A nor Player B is considered to have won, then add
 # 0.01 to the score if Player B has at least two more pieces than Player A
 # and subtract 0.01 from the score in Player A has at least two more pieces
@@ -48,6 +51,19 @@ from board_hardware import perform
 
 # Constants indicating the players involved.
 a, b = 'A', 'B'
+
+# There are three functions below that run this program. alphabeta returns an
+# optimal move (move = moving a piece or dropping a piece) whereas maximizer
+# and minimizer return integers representing scores. Don't directly call
+# maximizer or minimizer, call alphabeta. Maximizer and minimizer recursively
+# call each other and run when alphabeta is called. alphabeta has additional
+# code for randomizing optimal moves. In alphabeta, the randomization is
+# performed in such a way that the alpha-beta pruning algorithm is not
+# compromised.
+
+# This function represents the maximizing player, Player B, in the modified
+# alpha-beta pruning algorithm (alpha-beta pruning + skewing scores as
+# described above).
 
 def maximizer(pieces, depth, alpha, beta):
 
@@ -86,6 +102,9 @@ def maximizer(pieces, depth, alpha, beta):
 
     return max(maximinlist) + skew
 
+# This function represents the minimizing player, Player A, in the modified
+# alpha-beta pruning algorithm (alpha-beta pruning + skewing scores as
+# described above).
 
 def minimizer(pieces, depth, alpha, beta):
 
@@ -124,7 +143,11 @@ def minimizer(pieces, depth, alpha, beta):
 
     return min(minimaxlist) + skew
 
-# Depth should be at least 1.
+# Call this function when running the modified alpha-beta pruning algorithm
+# (alpha-beta pruning + skewing scores as described above + move randomization).
+# Depth should be at least 1. The data structure this function returns is the
+# data structure described in board.py that represents the game board and the
+# pieces.
 
 def alphabeta(pieces, depth, alpha = -math.inf, beta = +math.inf):
 
